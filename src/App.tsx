740b3514e90fa4dc0ee829bb9ad417552b49b467
Adrian <hannsadrian@gmail.com>
2021-10-06 10:33:57 +0200

    Update components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,46 @@
-import React from 'react';
-import "./App.css";
-
+import './App.css'
+import './tailwind.css'
 
 function App() {
   return (
-    <div className="background-beach bg-fixed h-full pb-64">
-      <header className="container px-3 pb-32 h-full sm:mx-auto min-h-screen">
-        <div style={{ height: "20vh" }}></div>
-        <div className="bg-pink-100 p-5 pb-6 sm:p-10 sm:pb-12 rounded-lg shadow-2xl sm:max-w-lg mx-auto">
+    <div className="h-full pb-64 bg-fixed background-beach">
+      <header className="container h-full min-h-screen px-3 pb-32 sm:mx-auto">
+        <div style={{ height: '20vh' }}></div>
+        <div className="p-5 pb-6 mx-auto bg-pink-100 rounded-lg shadow-2xl sm:p-10 sm:pb-12 sm:max-w-lg">
           <h1 className="text-3xl font-bold">
-            <span className="text-5xl">🏝</span><br />
-            <span className="inline-block leading-tight bg-clip-text text-transparent bg-gradient-to-l from-pink-400 to-red-500 h-full">
+            <span className="text-5xl">🏝</span>
+            <br />
+            <span className="inline-block h-full leading-tight text-transparent bg-clip-text bg-gradient-to-l from-pink-400 to-red-500">
               Days until Summer Holidays
             </span>
           </h1>
-          <p className="text-gray-600">The one question every student (and teacher?) occasionally comes across is finally answered!</p>
-          <hr className="mb-3 mt-4 border-1 rounded-lg overflow-hidden bg-pink-200 border-pink-200 border-2"/>
-          <h3 className="text-lg font-semibold mb-1">Some Questions first</h3>
+          <p className="text-red-400 opacity-75">
+            The one question every student (and teacher?) occasionally comes across is finally
+            answered!
+          </p>
+          <hr className="mt-4 mb-3 overflow-hidden bg-pink-200 border-2 border-pink-200 rounded-lg border-1" />
+          {/**<h3 className="mb-1 text-lg font-semibold text-red-600">Some Questions first</h3>
           <label className="flex items-center">
-            <input type="checkbox" className="form-checkbox" />
+            <input type="checkbox" className="text-blue-600 border-0 rounded-md" />
             <span className="ml-2">Do you only go to school on weekdays?</span>
           </label>
           <label className="flex items-center">
             <input type="checkbox" className="form-checkbox" />
             <span className="ml-2">Does your school have extra days off?</span>
-          </label>
+  </label>**/}
 
-          <label className="flex items-center mt-2">
-            <input style={{maxWidth: "110px"}} className="form-input block" placeholder="26.07.2020"/>
-            <span className="ml-2">When do your summer holidays start?</span>
+          <label className="flex items-center justify-between mt-2">
+            <span className="mr-2 text-pink-700">When do your summer holidays start?</span>
+            <input
+              style={{ maxWidth: '110px' }}
+              className="block px-1 placeholder-pink-300 rounded bg-pink-50"
+              placeholder="26.07.2020"
+            />
           </label>
         </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
